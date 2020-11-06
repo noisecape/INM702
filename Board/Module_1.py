52a9669af2695a1b51f5a932e4404ccd8b4c73b0
Tommaso Capecchi <tommycaps@hotmail.it>
2020-11-06 19:57:22 +0000

    Basic board setup

--- Board/Module_1.py
+++ Board/Module_1.py
@@ -1,1 +1,27 @@
-print("Test 2")
+import numpy as np
+
+class Board:
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.__board = self.init_board()
+
+    def init_board(self):
+        board = [[]]
+        for row in self.__width:
+            for column in self.__column:
+                board[row][column] = np.random.randint(0,10)
+        return board
+
+
+    @property
+    def get_board(self):
+        return self.__board
+
+    def print_board(self):
+
+
+
+board = Board()
+board.prin
