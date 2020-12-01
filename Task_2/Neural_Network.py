f5c152f69a89385f0488982aba2ba209666df197
Tommaso Capecchi <tommycaps@hotmail.it>
2020-12-01 17:49:31 +0000

    Changed Scheme of the Task2 package

--- Task_2/Neural_Network.py
+++ Task_2/Neural_Network.py
@@ -1,9 +1,66 @@
-import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from Task_2 import Dataset
 import time
+from torchvision.datasets import MNIST
+import numpy as np
+
+class Dataset:
+    """
+    The class that wraps the MNIST dataset downloaded and loaded by pytorch-vision.
+    Each digit is represented as a vector of 28x28 pixels, where each pixel can have values between 0-255.
+    In total there are 60000 elements in the training set while the elements in the test set are 10000.
+    """
+    def __init__(self):
+        train_loader = MNIST(root='.', download=True)
+        test_loader = MNIST(root='.', train=False)
+
+        # self.__train_data = self.__reshape_ditigs_matrix(train_loader.train_data.numpy())
+        # self.__train_labels = self.__reshape_labels(train_loader.train_labels.numpy())
+        # self.__test_data = self.__reshape_ditigs_matrix(test_loader.test_data.numpy())
+        # self.__test_labels = self.__reshape_labels(test_loader.test_labels.numpy())
+
+        self.debug_train_data = self.__reshape_ditigs_matrix(train_loader.train_data.numpy(), size=10000)
+        self.debug_train_labels = self.__reshape_labels(train_loader.train_labels.numpy(), size=10000)
+        self.debug_test_data = self.__reshape_ditigs_matrix(test_loader.test_data.numpy(), size=200)
+        self.debug_test_labels = self.__reshape_labels(test_loader.test_labels.numpy(), size=200)
+
+    def __reshape_ditigs_matrix(self, X, size=None):
+        """
+        :param X: the matrix to be reshaped each of shape(28x28)
+        :return: X_train reshaped with size (784x1)
+        """
+
+        if size:
+            reduced_X = np.reshape([(x/255) for x in X[:size]], (784, size))
+            return reduced_X
+        X = np.reshape([x/255 for x in X], (784, X.shape[0]))
+        return X
+
+    def __reshape_labels(self, labels, size=60000):
+        vectorized_labels = np.zeros((10, size))
+        for vector, digit in zip(vectorized_labels.transpose(), labels[:size]):
+            vector[digit] = 1
+        return vectorized_labels
+
+    @property
+    def train_data(self):
+        return self.__train_data
+
+    @property
+    def train_labels(self):
+        return self.__train_labels
+
+    @property
+    def test_data(self):
+        return self.__test_data
+
+    @property
+    def test_labels(self):
+        return self.__test_labels
+
+    def get_item(self, index):
+        return self.__train_data[index]
+
+
 
 class Layer:
 
@@ -23,7 +80,6 @@ class CrossEntropy:
     @staticmethod
     def compute_loss(y_pred, y):
         N = y.shape[1]
-        y_pred = np.clip(y_pred, 1e-12, 1. - 1e-12)
         error = -np.sum(y * np.log(y_pred))/N
         return error
 
@@ -39,7 +95,6 @@ class Softmax:
         # partial = z-maxes
         # num = np.exp(partial)
         # den = np.sum(num, axis=0, keepdims=True)
-
 
 
 class Sigmoid:
@@ -116,7 +171,7 @@ class NeuralNetwork:
         self.bias.append(np.zeros((1, 1)))
         self.Z.append(np.zeros((1, 1)))
         for l in range(len(self.layers)-1):
-            w = np.array(np.random.randn(self.layers[l+1].n_neurons, self.layers[l].n_neurons))
+            w = np.array(np.random.uniform(-1, 1, (self.layers[l+1].n_neurons, self.layers[l].n_neurons)))
             b = np.array(np.random.randn(self.layers[l+1].n_neurons, 1))
             self.weights.append(w)
             self.bias.append(b)
@@ -226,18 +281,18 @@ class StochasticGradientDescent(NeuralNetwork):
         print(f'Cost history; {loss_history}')
         print(f'Time to finish: {time.process_time()}')
 
-dataset = Dataset.Dataset()
-X_train = dataset.debug_train_data
-y_train = dataset.debug_train_labels
-X_test = dataset.debug_test_data
-y_test = dataset.debug_test_labels
-# X_test = X_train
-# y_test = y_train
-
-net = StochasticGradientDescent()
-net.add_layer(Layer(784, input_layer=True))
-net.add_layer(Layer(800, activation=Sigmoid()))
-net.add_layer(Layer(10, activation=Softmax()))
-net.compile(loss=CrossEntropy())
-net.fit(X_train, y_train, epochs=90, lr=0.01, lamda=0.1)
-net.predict(X_test, y_test)
+# dataset = Dataset()
+# X_train = dataset.debug_train_data
+# y_train = dataset.debug_train_labels
+# X_test = dataset.debug_test_data
+# y_test = dataset.debug_test_labels
+# # X_test = X_train
+# # y_test = y_train
+#
+# net = StochasticGradientDescent()
+# net.add_layer(Layer(784, input_layer=True))
+# net.add_layer(Layer(64, activation=ReLU()))
+# net.add_layer(Layer(10, activation=Softmax()))
+# net.compile(loss=CrossEntropy())
+# net.fit(X_train, y_train, epochs=90, lr=0.01, lamda=0.1)
+# net.predict(X_test, y_test)
